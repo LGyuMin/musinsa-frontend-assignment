73fbeb86ec4830fac32c927ee4d308b99d63eb36
규.이규민 <gmlee@lunasoft.co.kr>
2023-02-25 15:30:56 +0900

    프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>무신사 과제</h1>
+    </div>
+  )
+}
+
+export default App
