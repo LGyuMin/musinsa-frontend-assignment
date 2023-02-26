f67c65018904663feac431aa6900019b1d845446
규.이규민 <gmlee@lunasoft.co.kr>
2023-02-26 17:54:18 +0900

    리코일 스크립트 작업

--- src/atoms/CharacterList.tsx
+++ src/atoms/CharacterList.tsx
@@ -15,7 +15,8 @@ export const FilterAtom = atom<IFilter>({
     }
 })
 
-interface IList {
+export interface IListItem {
+    id: string
     name: string
     aliases: string[]
     title: string[]
@@ -23,4 +24,55 @@ interface IList {
     tvSeries: number
     gender: string
     died: string
+    deleted: boolean
 }
+
+const test_data: IListItem[] = [
+    { 
+        id: '0',
+        name: 'Balon Greyjoy',
+        aliases: [
+            "Balon the Brave",
+            "Balon the Blessed",
+            "Balon the Twice Crowned",
+            "Balon the Widowmaker",
+            "The Kraken King"
+        ],
+        title: [
+            "Iron King",
+            "King of the Isles and the North",
+            "King of Salt and Rock",
+            "Son of the Sea Wind",
+            "Lord Reaper of Pyke",
+            "The Greyjoy",
+            "Captain of the Great Kraken"
+        ],
+        books: 5,
+        tvSeries: 3,
+        gender: 'Male',
+        died: '',
+        deleted: false
+    }
+]
+
+export const ListAtom = atom<IListItem[]>({
+    key: 'list_atom',
+    default: [] as IListItem[]
+    // default: test_data
+})
+
+export const FilteredList = selector({
+    key: 'filtered_list',
+    get: ({get}) => {
+        const filter = get(FilterAtom);
+        let filteredList = get(ListAtom);
+
+        filteredList = filteredList.filter(item => !item.deleted) 
+        // TODO: 리팩토링 필요
+        if (filter.survived) filteredList= filteredList.filter(item => item.died !== '')
+        if (filter.female) filteredList= filteredList.filter(item => item.gender === 'Female')
+        if (filter.noTvSeries) filteredList= filteredList.filter(item => item.tvSeries === 0)
+
+        return filteredList;
+    }
+})
