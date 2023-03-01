ecff74b4318e0fbf50597afa7504ff2d54e9c0de
규.이규민 <gmlee@lunasoft.co.kr>
2023-03-01 14:13:57 +0900

    리스트 아이템 반응형 작업

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -14,7 +14,7 @@ const StyledDiv = styled.div`
 
     .info {
         width: 75%;
-        font-size: 18px;
+        font-size: 1.8rem;
         display: grid;
         grid-template-columns: 1fr 1fr;
         gap: 15px 5px;
@@ -29,12 +29,12 @@ const StyledDiv = styled.div`
     }
 
     button {
-        width: 90px;
-        height: 40px;
+        width: 9rem;
+        height: 4rem;
         border: 2px solid #000;
         background: #fff;
         color: #000;
-        font-size: 16px;
+        font-size: 1.6rem;
         font-weight: 700;
         border-radius: 20px;
         cursor: pointer;
